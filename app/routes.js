7225cd4054462332165ef6c4950f77eb020928a8
Fredrik <studiefredfredrik@gmail.com>
2015-12-03 21:15:47 +0100

    Removed some surplus code

--- app/routes.js
+++ app/routes.js
@@ -26,126 +26,122 @@ module.exports = function(app, passport) {
 		res.json({user: req.user});
     });
 
-// =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
 
-        // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+    // locally --------------------------------
+    // LOGIN ===============================
+    // show the login form
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/WWW', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/WWW', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // SIGNUP =================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/WWW', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+// facebook -------------------------------
+
+    // send to facebook to do the authentication
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
         }));
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+// twitter --------------------------------
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/WWW', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
+    // send to twitter to do the authentication
+    app.get('/auth/twitter', passport.authenticate('twitter', { scope : 'email' }));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
         }));
 
-    // facebook -------------------------------
 
-        // send to facebook to do the authentication
-        app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+// google ---------------------------------
 
-        // handle the callback after facebook has authenticated the user
-        app.get('/auth/facebook/callback',
-            passport.authenticate('facebook', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
+    // send to google to do the authentication
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-    // twitter --------------------------------
-
-        // send to twitter to do the authentication
-        app.get('/auth/twitter', passport.authenticate('twitter', { scope : 'email' }));
-
-        // handle the callback after twitter has authenticated the user
-        app.get('/auth/twitter/callback',
-            passport.authenticate('twitter', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
-
-
-    // google ---------------------------------
-
-        // send to google to do the authentication
-        app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-        // the callback after google has authenticated the user
-        app.get('/auth/google/callback',
-            passport.authenticate('google', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
+        }));
 
 // =============================================================================
 // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
 // =============================================================================
 
-    // locally --------------------------------
-        app.get('/connect/local', function(req, res) {
-            res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-        });
-        app.post('/connect/local', passport.authenticate('local-signup', {
-            successRedirect : '/WWW', // redirect to the secure profile section
-            failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
+// locally --------------------------------
+    app.get('/connect/local', function(req, res) {
+        res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect : '/WWW', // redirect to the secure profile section
+        failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+// facebook -------------------------------
+
+    // send to facebook to do the authentication
+    app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authorized the user
+    app.get('/connect/facebook/callback',
+        passport.authorize('facebook', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
         }));
 
-    // facebook -------------------------------
+// twitter --------------------------------
 
-        // send to facebook to do the authentication
-        app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+    // send to twitter to do the authentication
+    app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
 
-        // handle the callback after facebook has authorized the user
-        app.get('/connect/facebook/callback',
-            passport.authorize('facebook', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
+    // handle the callback after twitter has authorized the user
+    app.get('/connect/twitter/callback',
+        passport.authorize('twitter', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
+        }));
 
-    // twitter --------------------------------
+    // google ---------------------------------
+    // send to google to do the authentication
+    app.get('/connect/google', passport.authorize('google', { scope : ['profile', 'email'] }));
 
-        // send to twitter to do the authentication
-        app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+    // the callback after google has authorized the user
+    app.get('/connect/google/callback',
+        passport.authorize('google', {
+            successRedirect : '/WWW',
+            failureRedirect : '/'
+        }));
 
-        // handle the callback after twitter has authorized the user
-        app.get('/connect/twitter/callback',
-            passport.authorize('twitter', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
-
-        // google ---------------------------------
-        // send to google to do the authentication
-        app.get('/connect/google', passport.authorize('google', { scope : ['profile', 'email'] }));
-
-        // the callback after google has authorized the user
-        app.get('/connect/google/callback',
-            passport.authorize('google', {
-                successRedirect : '/WWW',
-                failureRedirect : '/'
-            }));
-
-// =============================================================================
 // UNLINK ACCOUNTS =============================================================
-// =============================================================================
 // used to unlink accounts. for social accounts, just remove the token
 // for local account, remove email and password
 // user account will stay active in case they want to reconnect in the future
@@ -194,6 +190,7 @@ module.exports = function(app, passport) {
 	    var path = require("path");
 	    var fs = require('fs');
 	    var isAdminUser = false;
+        // File exist, there is a tree to get
 	    fs.exists(path.resolve(__dirname) + "/json/" + req.user._id, function (exists) {
 	        if (exists) {
 	            fs.readFile(path.resolve(__dirname) + "/json/" + req.user._id + "ISADMINUSER", 'utf8', function (err, data) {
@@ -211,7 +208,8 @@ module.exports = function(app, passport) {
 	                    res.send(r);
 	                });
 	            });
-	        } 
+	        }
+	        // If no file exist, create tree
 	        else {
 	            fs.writeFile(path.resolve(__dirname) + "/json/" + req.user._id, JSON.stringify({
                     name: 'Click here to start building a tree, If you are logged in as',
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// server.js
-
-// set up ======================================================================
+// setup ======================================================================
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 10080;
@@ -11,36 +9,31 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session = require('express-session');
-
 var multer = require('multer'); // File upload
 var done = false;
-
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 require('./config/passport')(passport); // pass passport for configuration
 
-// set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'slektstresessionsecret' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// File upload stuff - start
+// File upload
 var newFilename = 'error maybe';
 var newFilePath = 'error maybe';
 var fileDestinationFolder = './WWW/uploads/';
 
-// File upload
 app.use(multer({
     dest: fileDestinationFolder,
     rename: function (fieldname, filename) {
@@ -57,7 +50,7 @@ app.use(multer({
     }
 }));
 
-// routes ======================================================================
+// routes static  ======================================================================
 app.post('/api/photo', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     if (done == true) {
